8a250007b7624114ebfefec8b704edaacf39e456
Vladyslav Okopnyy <harmfulcati@gmail.com>
2021-04-08 18:34:05 +0300

    Esercizio Finito

--- js/script.js
+++ js/script.js
@@ -5,9 +5,32 @@ function initVue() {
     el: '#app',
     data: {
 
-      
+      'todoList':['Palestra', 'Fare la Spesa', 'Passegiare con cane'],
+      'newTodo': ''
+
+
+    },
+    methods: {
+
+      deleteTodo: function (index) {
+
+        this.todoList.splice(index, 1);
+      },
+
+      addTodo: function () {
+
+        if (this.newTodo.length > 0) {
+
+          this.todoList.push(this.newTodo);
+        }
+
+        this.newTodo = '';
+      }
+
     }
+
   });
+  
 }
 
 function init() {
