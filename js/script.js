fed905ec96540cf77b1abe40edebbfa702b24e67
Vladyslav Okopnyy <harmfulcati@gmail.com>
2021-04-08 16:32:10 +0300

    Inizio Esercizio

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function initVue() {
+
+  new Vue({
+
+    el: '#app',
+    data: {
+
+      
+    }
+  });
+}
+
+function init() {
+  initVue();
+}
+
+document.addEventListener('DOMContentLoaded', init);
